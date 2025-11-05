93e69a6bb1838159ea774b2338d8e113c7f8b484
Winston Nji <winston01russel@gmail.com>
2025-11-05 22:25:50 +0200

    updated base url

--- src/lib/getBaseUrl.js
+++ src/lib/getBaseUrl.js
@@ -1,6 +1,6 @@
 export function getBaseUrl() {
   if (process.env.NODE_ENV === "production") {
-    return "https://wiyorent-student-referral-xwju.vercel.app/";
+    return "wiyorent-student-referral.vercel.app";
   }
   return "http://localhost:3000";
 }
